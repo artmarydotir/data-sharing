19db987ab0abf9b7b5ab66609553945e70ed20bc
Art Mary <artmary.ir@gmail.com>
2019-05-27 13:20:09 +0430

    comunicate data.

---
+++ src/components/User.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <button @click="changeName"> change my name </button>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail :userAge="age" :name="name" @nameToBeChange="name = $event" ></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit :userAge="age"></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data: function() {
+            return {
+                name : 'just mary',
+                age : 25,
+            };
+        },
+        components : { 
+            appUserDetail : UserDetail,
+            appUserEdit : UserEdit
+        },
+        methods: {
+            changeName() {
+                this.name = 'Art Mary'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ src/components/UserDetail.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User name : {{ name }} </p>
+        <p>User age : {{ userAge }} </p>
+        <p> name uppercase: {{ changepropName() }} </p>
+        <button @click="resetName"> reset me </button>
+    </div>
+</template>
+
+<script>
+import { eventBus } from '../main.js';
+
+export default {
+    props: {
+        name : String,
+        userAge : Number
+    },
+    methods: {
+        changepropName() {
+            return this.name.toUpperCase();
+        },
+        resetName() {
+            this.name = 'Max';  
+            this.$emit('nameToBeChange', this.name);
+        }
+    },
+    created() {
+        eventBus.$on('editAgeSibilings', (data)=> {
+            this.userAge = data;
+        });
+    }
+}
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
